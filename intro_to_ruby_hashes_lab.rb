d43e69307636c05af30475d7d7198d59a6ab2122
Ryan Pethel <arpethel@gmail.com>
2019-02-22 09:10:22 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,7 +25,7 @@ def monopoly_with_third_tier
 	  :railroads=>
 	  {
 	    :pieces=>4,
-	    :names=>{}
+	    :names=>new_hash
 	  }
 	}
 end
@@ -36,8 +36,8 @@ def monopoly_with_fourth_tier
 	  :railroads=>
 	  {
 	    :pieces=>4,
-	    :rent_in_dollars=>{},
-	    :names=>{}
+	    :rent_in_dollars=>new_hash,
+	    :names=>new_hash
 	  }
 	}
 end
